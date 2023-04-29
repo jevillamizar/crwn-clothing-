16143035d669615aa9350d126bc5685e2116c37f
jevillamizar <jevillamizar@unicauca.edu.co>
2023-04-28 23:57:14 -0500

    $ git commit -a

--- src/routes/sing-in/sing-in.component.jsx
+++ src/routes/sing-in/sing-in.component.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { signInWithGoolePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
@@ -12,6 +13,27 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>Sing in with Google popup</button>
         </div>
     );
+=======
+import {
+  signInWithGoolePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGoolePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <div>
+      <h1>Sing In Page</h1>
+      <button onClick={logGoogleUser}>Sing in with Google popup</button>
+      
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default SignIn;
