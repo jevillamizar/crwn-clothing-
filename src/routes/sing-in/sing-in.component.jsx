9a5fb71ad3e44999d3387e1b26585f576e7f7319
jevillamizar <jevillamizar@unicauca.edu.co>
2023-04-29 00:01:27 -0500

    added our sing in component continue

--- src/routes/sing-in/sing-in.component.jsx
+++ src/routes/sing-in/sing-in.component.jsx
@@ -1,19 +1,4 @@
-<<<<<<< Updated upstream
-import { signInWithGoolePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
-const SignIn = () => {
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGoolePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
-
-    return (
-        <div>
-            <h1>Sing In Page</h1>
-            <button onClick={logGoogleUser}>Sing in with Google popup</button>
-        </div>
-    );
-=======
 import {
   signInWithGoolePopup,
   createUserDocumentFromAuth,
@@ -33,7 +18,6 @@ const SignIn = () => {
       
     </div>
   );
->>>>>>> Stashed changes
 };
 
 export default SignIn;
